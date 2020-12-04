f8d89d577fe46cd3616c845b66dcc65396c4ed43
jukimura <juliazinha.kimura@gmail.com>
2020-12-04 11:21:04 -0300

    Cliente

---
+++ Cliente/src/cliente/Cliente.java
@@ -0,0 +1,12 @@
+package cliente;
+
+import java.net.*;
+import java.io.*;
+
+public class Cliente 
+{
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ Cliente/src/cliente/ComunicadoDeDesligamento.java
@@ -0,0 +1,4 @@
+package cliente;
+
+public class ComunicadoDeDesligamento extends Comunicado
+{}

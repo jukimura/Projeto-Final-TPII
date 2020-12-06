2d08800248467d96f3848c805647a261b5dde4a9
gustavoWaki <75136535+gustavoWaki@users.noreply.github.com>
2020-12-06 09:59:01 -0300

    Servidor

---
+++ Servidor/Carta.java
@@ -0,0 +1,49 @@
+public class Carta {
+    private String texto;
+    private int valor;
+
+    public Carta(String texto, int valor){
+        this.texto = texto;
+        this.valor = valor;
+    }
+
+    public int getValor(){
+        return valor;
+    }
+
+    public String toString(){
+        return texto;
+    }
+
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+
+        if (obj == null) return false;
+
+        if (this.getClass()!=obj.getClass()) return false;
+
+        Carta x = (Carta)obj;
+
+        if(this.texto == x.texto) return true;
+
+        return false;
+    }
+
+    public int HashCode(){
+        int random = 371;
+
+        random = 7 * random + new Integer ((int)this.texto.charAt(1)).hashCode();
+        random = 11 * random + new Integer ((int)this.texto.charAt(0)).hashCode();
+        random = 13 * random + new Integer (this.valor).hashCode();
+
+        if (random < 0) random = -random;
+
+        return random;
+    }
+
+
+    public int compareTo(Carta carta){
+        return this.texto.compareTo(carta.texto);
+    }
+
+}
---
+++ Servidor/Partida.java
@@ -0,0 +1,9 @@
+public class Partida extends Thread
+{
+    private Baralho baralho = new Baralho();
+
+    public void run()
+    {
+        
+    }
+}
